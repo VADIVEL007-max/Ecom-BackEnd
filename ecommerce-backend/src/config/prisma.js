dd10def52ced8f08e1776531295f1d162d20d4a8
VADIVEL007-max <vadivels759@gmail.com>
2026-06-16 22:49:12 +0530

    add about the  config file comment

--- ecommerce-backend/src/config/prisma.js
+++ ecommerce-backend/src/config/prisma.js
@@ -3,3 +3,6 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = prisma;
+
+// Prisma Configuration
+// Creates a single Prisma client instance
