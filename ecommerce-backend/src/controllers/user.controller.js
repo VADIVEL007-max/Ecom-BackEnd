cb3e9cb009768239eda15c8330a651b5e17bde28
VADIVEL007-max <vadivels759@gmail.com>
2026-06-17 14:01:56 +0530

    Hash passwords and hide sensitive user data

--- ecommerce-backend/src/controllers/user.controller.js
+++ ecommerce-backend/src/controllers/user.controller.js
@@ -1,18 +1,29 @@
  
 const prisma = require("../config/prisma");
 const newuser = require("../services/user.service");
+const bcrypt = require("bcrypt");
 
 
-
+// add a new user in db
 const newregister= async(req,res)=>{
+// get the all data using body so use destructure method
     try {
         const {name,email,password}=req.body;
-            if (!name || !email || !password) {
-                return res.status(400).json({ message: "All fields are required"});
-}
-        const registernew=await newuser({name,email,password});
+//    now checkt any feild is empty and sendd responces
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "All fields are required"});
+        }
 
-        res.status(201).json({message:"new user added successfully...",data: registernew})
+// hash the pass  using the bycrpt
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+//  now we send the data to service to acces so their name but pass is bycrypt pass //note this you make a mistake
+        const registernew=await newuser({name,email,password:hashedPassword});
+
+//dont need to send the sensitive datato frontend so get thhe pass then pack other userdata  using the spread operator
+        const {  password: dbPassword, ...userWithoutPassword } = registernew;
+
+        res.status(201).json({message:"new user added successfully...",data: userWithoutPassword})
         console.log("new user added successfully...");
         
         
