dece63874471e7e4f9b2e8dd38d6ce67b614eedc
VADIVEL007-max <vadivels759@gmail.com>
2026-06-17 14:38:15 +0530

    Add email validation to user registration

--- ecommerce-backend/src/controllers/user.controller.js
+++ ecommerce-backend/src/controllers/user.controller.js
@@ -1,6 +1,4 @@
- 
-const prisma = require("../config/prisma");
-const newuser = require("../services/user.service");
+const {findUserByEmail,newuser} = require("../services/user.service");
 const bcrypt = require("bcrypt");
 
 
@@ -12,20 +10,28 @@ const newregister= async(req,res)=>{
 //    now checkt any feild is empty and sendd responces
         if (!name || !email || !password) {
             return res.status(400).json({ message: "All fields are required"});
-        }
+        };
 
-// hash the pass  using the bycrpt
+        const existingUser = await findUserByEmail(email);
+// user exsit send error message
+        if (existingUser) {
+                return res.status(400).json({ message: "Email already exists" });
+        };
+
+        // hash the pass  using the bycrpt
         const hashedPassword = await bcrypt.hash(password, 10);
 
-//  now we send the data to service to acces so their name but pass is bycrypt pass //note this you make a mistake
+        //  now we send the data to service to acces so their name but pass is bycrypt pass //note this you make a mistake
         const registernew=await newuser({name,email,password:hashedPassword});
 
-//dont need to send the sensitive datato frontend so get thhe pass then pack other userdata  using the spread operator
+        //dont need to send the sensitive datato frontend so get thhe pass then pack other userdata  using the spread operator
         const {  password: dbPassword, ...userWithoutPassword } = registernew;
 
         res.status(201).json({message:"new user added successfully...",data: userWithoutPassword})
-        console.log("new user added successfully...");
-        
+        console.log("new user added successfully...");     
+
+       
+
         
     } catch (error) {
          console.log(error);
--- ecommerce-backend/src/services/user.service.js
+++ ecommerce-backend/src/services/user.service.js
@@ -2,10 +2,21 @@
 
 const prisma = require("../config/prisma");
 
+
+
+// check the email is alread exsits
+const findUserByEmail = async (email) => {
+  return await prisma.user.findUnique({
+    where: {
+      email:email
+    }
+  });
+};
+// 
 const newuser=async(userdata)=>{
     return await prisma.user.create({
          data: userdata       
         })
 }
 
-module.exports=newuser;
+module.exports={newuser,findUserByEmail};
