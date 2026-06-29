47243066535d04c60edbefe5f8fb8b84239fc5e7
VADIVEL007-max <vadivels759@gmail.com>
2026-06-29 10:31:40 +0530

    category api updated

--- ecommerce-backend/src/controllers/category.controller.js
+++ ecommerce-backend/src/controllers/category.controller.js
@@ -37,8 +37,7 @@ const getProducts = async (req, res) => {
 
     const products = await getAllProducts();
 
-    res.status(200).json({
-      data: products
+    res.status(200).json({data: products.category
     });
 
   } catch (error) {
